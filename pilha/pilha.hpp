9ba583d05717dea66a25b1c29bbaf64dd7e86a49
felcorrea20 <felipe.correa2@outlook.com>
2026-09-15 08:32:14 -0300

    refactor(pilhas): Separar arquivo de pilha com cabeçalho e arquivo main

--- pilha/pilha.hpp
+++ pilha/pilha.hpp
@@ -1,8 +1,6 @@
 #ifndef PILHA_HPP
 #define PILHA_HPP
 
-#include <iostream>
-
 using namespace std;
 
 class Pilha {
@@ -12,52 +10,14 @@ private:
     int topo;
 
 public:
-    Pilha() 
-    {
-        vet = new int[100];
-        max_tam = 99;
-        topo = -1;
-    }
+    Pilha();
+    ~Pilha();
 
-    ~Pilha()
-    {
-        delete[] vet;
-    }
-
-    void empilhar(int e) 
-    {
-        if (topo == max_tam)
-            cout << "Pilha cheia" << endl;
-        else
-            vet[++topo] = e;
-    }
-
-    void desempilhar()
-    {
-        if (isVazio())
-            cout << "Pilha vazia" << endl;
-        else 
-            vet[topo--] = NULL;
-    }
-
-    int isVazio()
-    {
-        return (topo == -1);
-    }
-
-    void mostraElementoTopo()
-    {
-        if (isVazio()) 
-            cout << "Pilha vazia" << endl;
-        else
-            cout << "Elemento topo: " << this->vet[this->topo] << endl;
-    }
-
-    int getQuantidadeElementos()
-    {
-        return (topo + 1);
-    }
-
+    void empilhar(int e);
+    void desempilhar();
+    int isVazio();
+    void mostraElementoTopo();
+    int getQuantidadeElementos();
 };
 
 #endif // PILHA_HPP
